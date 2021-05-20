3f164af580060eeae671a54d28d9f2063658195b
tascord <theactivesquare@gmail.com>
2021-05-20 21:09:18 +1000

    test

--- bootstrap.js
+++ bootstrap.js
@@ -50,8 +50,8 @@ const start_brc = (port) => {
 
     });
 
-    app.post('/update', (req, res) => {
-        console.log(grey('(') + cyanBright('BRC') + grey(') ') + greenBright('Update request ') + whiteBright(req.path + ' ') + grey(req.body));
+    app.post('*', (req, res) => {
+        console.log(grey('(') + cyanBright('BRC') + grey(') ') + greenBright('POST request ') + whiteBright(req.path + ' ') + grey(req.body));
         res.status(204).end();
     })
 
